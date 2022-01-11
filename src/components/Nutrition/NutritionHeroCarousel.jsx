bcd0635c8b0a27467fe32f690fdf1bf9cf6a0d55
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2022-01-11 22:39:29 +0530

    ...

--- src/components/Nutrition/NutritionHeroCarousel.jsx
+++ src/components/Nutrition/NutritionHeroCarousel.jsx
@@ -7,8 +7,8 @@ import { NextArrow, PrevArrow } from "../CarouselArrow";
 
 function NutritionHeroCarousel() {
   const [heroBanners, setHeroBanners] = useState([
-    "s://b.zmtcdn.com/web/nutrition/assets/47fb421f35ca12ad3111e3d99d1737571620108828.png",
-    "s://b.zmtcdn.com/web/nutrition/assets/0a8f2dad65904b89178905213efe886c1620108711.png",
+    "https://m.media-amazon.com/images/S/stores-image-uploads-eu-prod/0/AmazonStores/A21TJRUUN4KGV/4fdcaabd5545969580759caaa3f22a53.w6000.h1200._SL5000_CR0%2C0%2C5000%2C1000_SX1920_.png",
+    "https://m.media-amazon.com/images/S/aplus-media-library-service-media/b484e636-3ed8-4960-a34e-e48a2bf9b0b9.__CR0,0,970,600_PT0_SX970_V1___.jpg",
     "https://b.zmtcdn.com/web/nutrition/assets/f8da550c375c3bec2f6ba341336324561620108741.png",
     "https://b.zmtcdn.com/web/nutrition/assets/cfbb36a56a4203c7efac5de27318ea381620108756.png",
     "https://b.zmtcdn.com/web/nutrition/assets/3872dc3041e9633ba40b51e7dbff263a1620108770.png",
