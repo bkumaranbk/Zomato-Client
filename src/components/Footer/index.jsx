5b1e3dcc03b0ba0fa1709c265e000444673eb563
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2022-01-15 15:52:23 +0530

    ...

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
 function LargeFooter() {
-    const dispatch = useDispatch();
     return (
         <footer className="border-t border-gray-200 lg:p-12">
         <div>
@@ -145,7 +144,6 @@ function MobileFooter() {
 
 
 function Footer () {
-    const dispatch = useDispatch();
     return (
         <>
         <div  className="flex w-full items-center justify-between lg:hidden">
